563c5a7fb8037dcd7f602f64b3c463dbfd968260
Liang, Shuang (Icer) <liangshuang@sutu.tech>
2025-09-14 18:09:11 +0800

    init github actions for pages

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://wizicer.github.io/zkVSA',
+  base: '/zkVSA',
   vite: {
     plugins: [tailwindcss()]
   }
