3a816d8db3e4145ae0421e3274a5cbdc1d3adca1
Liang, Shuang (Icer) <liangshuang@sutu.tech>
2025-09-18 10:28:38 +0800

    simplify sample.json by convension

--- docs/src/data/types.ts
+++ docs/src/data/types.ts
@@ -1,31 +1,5 @@
 // Type definitions for ZK-VSA samples
-export interface AnonymizedSpeech {
-  f32: string;
-  m32: string;
-  m377: string;
-}
-
-export interface Proof {
-  size: string;
-}
-
 export interface Sample {
   transcript: string;
-  originalSpeech: string;
-  anonymizedSpeech: {
-    "3": AnonymizedSpeech;
-    "2": AnonymizedSpeech;
-    "1": AnonymizedSpeech;
-    "-1": AnonymizedSpeech;
-    "-2": AnonymizedSpeech;
-    "-3": AnonymizedSpeech;
-  };
-  proof: {
-    "3": Proof;
-    "2": Proof;
-    "1": Proof;
-    "-1": Proof;
-    "-2": Proof;
-    "-3": Proof;
-  };
+  name: string;
 }
